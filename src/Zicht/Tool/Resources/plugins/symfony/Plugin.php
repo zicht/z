c63cc24b88dbc806486948fa382d300e36643e84
Gerard van Helden <gerard@zicht.nl>
2013-01-15 17:47:20 +0000

    config symfony plugin, added web and console paths
    git-svn-id: svn://zon/projecten/zicht-libraries/z/trunk@15265 0f35748b-8228-47f0-bbea-889afbbd9a42

---
+++ src/Zicht/Tool/Resources/plugins/symfony/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Gerard van Helden <gerard@zicht.nl>
+ * @copyright Zicht Online <http://zicht.nl>
+ */
+
+namespace Zicht\Tool\Plugin\Symfony;
+
+use \Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+use \Zicht\Tool\Plugin as BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    public function appendConfiguration(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('symfony')
+                    ->children()
+                        ->scalarNode('console')->defaultValue('app/console')->end()
+                        ->scalarNode('web')->defaultValue('web')->end()
+                    ->end()
+                    ->addDefaultsIfNotSet()
+                ->end()
+            ->end()
+        ;
+    }
+}
