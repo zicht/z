2dbd401c473ced3dbf22f1b01a211d8312c4047b
Gerard van Helden <gerard@zicht.nl>
2013-03-08 20:10:23 +0000

    CS
    git-svn-id: svn://zon/projecten/zicht-libraries/z/trunk@17390 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Tool/Command/TaskCommand.php
+++ src/Zicht/Tool/Command/TaskCommand.php
@@ -5,8 +5,8 @@
  */
 
 namespace Zicht\Tool\Command;
-use \Symfony\Component\Console\Command\Command;
 
+use \Symfony\Component\Console\Command\Command;
 use \Symfony\Component\Console\Input\InputOption;
 use \Symfony\Component\Console\Input\InputInterface;
 use \Symfony\Component\Console\Output\OutputInterface;
