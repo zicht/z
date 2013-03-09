3efc709e6b3b1532a25fc80af2f62f3d6670d3cd
Gerard van Helden <gerard@zicht.nl>
2013-03-09 14:16:00 +0000

    CS
    git-svn-id: svn://zon/projecten/zicht-libraries/z/trunk@17394 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Tool/Command/BaseCommand.php
+++ src/Zicht/Tool/Command/BaseCommand.php
@@ -5,9 +5,6 @@
  */
 namespace Zicht\Tool\Command;
 
-use \Symfony\Component\DependencyInjection\ContainerInterface;
-use \Symfony\Component\Console\Input\InputInterface;
-use \Symfony\Component\Console\Output\OutputInterface;
 use \Symfony\Component\Console\Command\Command;
 use \Symfony\Component\Console\Input\InputOption;
 
