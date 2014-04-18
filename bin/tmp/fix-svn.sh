2e567c41e8604ac917d5d91077cdf759c0f90311
Gerard van Helden <gerard@zicht.nl>
2014-04-18 14:37:29 +0000

    removing svn fix script
    git-svn-id: svn://zon/projecten/zicht-libraries/z/trunk@35712 0f35748b-8228-47f0-bbea-889afbbd9a42

--- bin/tmp/fix-svn.sh
+++
@@ -1,9 +0,0 @@
-repo=$1
-reposrc=$1/z-plugins/trunk
-repotgt=$1/z-plugin-
-
-for plugin in $(svn ls $reposrc | egrep '/$' | sed 's!/$!!'); do
-    echo svn mkdir $repotgt$plugin
-    echo svn mv $reposrc/$plugin $repotgt$plugin/trunk;
-    echo svn mkdir $repotgt$plugin/{tags,branches}
-done;
