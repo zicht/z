cf57e266a464ca14ca0f4c9a3760a74a75008c67
Gerard van Helden <gerard@zicht.nl>
2015-03-06 10:17:48 +0000

    1to2 now produces a z2.yml
    git-svn-id: svn://zon/projecten/zicht-libraries/z/trunk@57808 0f35748b-8228-47f0-bbea-889afbbd9a42

--- bin/1to2.sh
+++ bin/1to2.sh
@@ -1,47 +1,46 @@
 set -e
 
 file=$1
+out=$2
 
 if [ "$file" == "" ]; then
-    echo "Usage: 1to2.sh FILE"
+    echo "Usage: 1to2.sh INFILE [OUTFILE]"
     exit 1;
 fi
+if [ "$out" == "" ]; then
+    out=z2.yml
+fi
+
 if ! [ -e "$file" ]; then
     echo "File does not exist: $file"
     exit 2;
 fi
 
-echo "Backing up $file to $file.1"
-if [ -e "$file.1" ]; then
-    echo "File exists! aborting";
-    exit 3
-fi
-
-cp $file $file.1
+cp $file $out
 
 echo "Replacing 'set:' directives with 'args:'"
-sed 's/set:/args:/g'                                                        -i $file;
+sed 's/set:/args:/g'                                                        -i $out;
 
 echo "Replacing 'env:' global setting with 'envs:'"
-sed 's/^env:/envs:/g'                                                       -i $file;
+sed 's/^env:/envs:/g'                                                       -i $out;
 
 echo "Replacing 'env:' parameters with 'target_env:'"
-sed 's/\(\s\)env:/\1target_env:/g'                                            -i $file;
+sed 's/\(\s\)env:/\1target_env:/g'                                            -i $out;
 
 echo "Replacing 'env.\*' expressions with 'envs[target_env].\*'"
-sed 's/env\./envs[target_env]\./g'                  -i $file;
+sed 's/env\./envs[target_env]\./g'                  -i $out;
 
 echo "Replacing 'env' expressions with 'target_env'"
-sed 's/\$(env)/$(target_env)/g'                                             -i $file;
+sed 's/\$(env)/$(target_env)/g'                                             -i $out;
 
 echo "Removing reference to core plugin"
-sed 's/\(plugins:.*\)\('"'"'core'"'"', \?\|, \?'"'"'core'"'"'\)/\1/g'       -i $file;
+sed 's/\(plugins:.*\)\('"'"'core'"'"', \?\|, \?'"'"'core'"'"'\)/\1/g'       -i $out;
 
 echo "Replacing 'verbose' with 'VERBOSE'"
-sed 's/\?(verbose)/?(VERBOSE)/g'                                          -i $file;
+sed 's/\?(verbose)/?(VERBOSE)/g'                                          -i $out;
 
 echo "Replacing ?(...) with @(if ...)"
-sed 's/\?(/@(if /g'                                          -i $file;
+sed 's/\?(/@(if /g'                                          -i $out;
 
 echo "Done"
 echo ""
