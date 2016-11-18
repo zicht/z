9da06cf616c1211f6e157c16a2f99827b6f6a98c
Gerard van Helden <gerard@zicht.nl>
2016-11-18 19:58:25 +0100

    more test cases

--- tests/ZichtTest/Tool/assets/parser/cur.php
+++ tests/ZichtTest/Tool/assets/parser/cur.php
@@ -1,34 +1,5 @@
 <?php
 
-/*
-vars:
-    a:
-        b:
-            c: "Ultimate"
-    x:
-        y:
-            z: "b"
-    foo:
-        bar:
-            baz: "c"
-
- */
 return [
-    'vars' => [
-        'a' => [
-            'b' => [
-                'c' => 'Ultimate',
-            ]
-        ],
-        'x' => [
-            'y' => [
-                'z' => 'b'
-            ],
-        ],
-        'foo' => [
-            'bar' => [
-                'baz' => 'c'
-            ]
-        ]
-    ],
+    'a' => ['x' => 1, 'y' => 2, 'z' => 3]
 ];
